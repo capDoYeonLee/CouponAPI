bf1aca1832b3d28ccc8ea8f835aba6bc09ae2df4
capDoYeonLee <ghtyu21@naver.com>
2023-07-24 19:36:34 +0900

    fix : UserRepository null값 수정

--- src/main/java/com/example/demo/sign/controller/UserController.java
+++ src/main/java/com/example/demo/sign/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 //@RequiredArgsConstructor
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
 
@@ -36,19 +35,16 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signup(@ModelAttribute SiteUser siteUser, UserCreateForm userCreateForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "signup_form";
-        }
+    public String signup(@ModelAttribute SiteUser siteUser) {
 
-        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
-            bindingResult.rejectValue("password2", "passwordInCorrect",
-                    "2개의 패스워드가 일치하지 않습니다.");
-            return "signup_form";
-        }
+//        if (!userCreateForm.setPassword1().equals(userCreateForm.setPassword2())) {
+//            bindingResult.rejectValue("password2", "passwordInCorrect",
+//                    "2개의 패스워드가 일치하지 않습니다.");
+//            return "signup_form";
+//        }
 
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(), userCreateForm.getPassword1());
+//        userService.create(userCreateForm.setUsername(),
+//                userCreateForm.setEmail(), userCreateForm.setPassword1());
 
         userCreateForm.saveUser(siteUser);
         return "redirect:/";
